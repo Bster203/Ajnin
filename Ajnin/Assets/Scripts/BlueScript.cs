aed0d442355dd88a5cbdab70ee6362324081d698
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-07-30 23:08:10 -0400

    starting AI

---
+++ Ajnin/Assets/Scripts/BlueScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueScript : MonoBehaviour
+{
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
+}
