1b2f16e868114ffe9a337a5670026a619ec8aa26
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-09-30 02:12:15 -0400

    cheese

---
+++ Ajnin/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    GameObject target;
+    public float speed;
+    Rigidbody2D bulletRB;
+    [SerializeField] private float damage;
+
+    void Start()
+    {
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+        Destroy(this.gameObject, 2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
