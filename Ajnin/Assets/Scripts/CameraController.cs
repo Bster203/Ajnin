15d45f69b5a84509549f7b78ba060362856c761f
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-10-05 22:54:39 -0400

    fun

--- Ajnin/Assets/Scripts/CameraController.cs
+++ Ajnin/Assets/Scripts/CameraController.cs
@@ -16,7 +16,7 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         //Room camera
-        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
 
         //Follow player
         transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
