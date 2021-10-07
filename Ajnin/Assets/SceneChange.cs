fb3f5fdc3ff39a8c31af2793c384afd9fa885613
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-10-06 23:29:36 -0400

    enhance

--- Ajnin/Assets/SceneChange.cs
+++ Ajnin/Assets/SceneChange.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Fall : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
-    
+    [SerializeField] private string sceneName;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
